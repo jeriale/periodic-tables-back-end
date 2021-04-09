c927a2c81e82af725d34b0cceaf4b93b28300405
Jon Belaire <jon.belaire@icloud.com>
2021-04-08 23:19:30 -0400

    routine save

--- src/server.js
+++ src/server.js
@@ -3,14 +3,14 @@ const { PORT = 5000 } = process.env;
 const app = require("./app");
 const knex = require("./db/connection");
 
-knex.seed
-  .run()
-  .then((seeds) => {
-    console.log("Seeded: ", seeds);
+knex.migrate
+  .latest()
+  .then((migrations) => {
+    console.log("Migrated: ", migrations);
     app.listen(PORT, listener)
   })
-  .catch(() => {
-    console.error();
+  .catch((error) => {
+    console.error(error);
     knex.destroy();
   });
 
