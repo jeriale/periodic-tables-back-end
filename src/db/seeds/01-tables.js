775884881dc14fecb6a7db5f187002a99675ec89
Jon Belaire <jon.belaire@icloud.com>
2021-04-10 04:27:10 -0400

    populated tables seed file with more tables

--- src/db/seeds/01-tables.js
+++ src/db/seeds/01-tables.js
@@ -5,11 +5,35 @@ exports.seed = function (knex) {
         return knex("tables")
           .insert([
             {
-                table_name: "#1",
+                table_name: "Band Room #1",
                 capacity: 6
             },
             {
-                table_name: "#2",
+                table_name: "Band Room #2",
+                capacity: 6
+            },
+            {
+                table_name: "Band Room #3",
+                capacity: 6
+            },
+            {
+                table_name: "Band Room #4",
+                capacity: 6
+            },
+            {
+                table_name: "Band Room #5",
+                capacity: 6
+            },
+            {
+                table_name: "Band Room #6",
+                capacity: 6
+            },
+            {
+                table_name: "Band Room #7",
+                capacity: 6
+            },
+            {
+                table_name: "Band Room #8",
                 capacity: 6
             },
             {
@@ -19,7 +43,47 @@ exports.seed = function (knex) {
             {
                 table_name: "Bar #2",
                 capacity: 1
-            }
+            },
+            {
+                table_name: "Bar #3",
+                capacity: 1
+            },
+            {
+                table_name: "Bar #4",
+                capacity: 1
+            },
+            {
+                table_name: "Bar #5",
+                capacity: 1
+            },
+            {
+                table_name: "Bar #6",
+                capacity: 1
+            },
+            {
+                table_name: "Patio #1",
+                capacity: 4
+            },
+            {
+                table_name: "Patio #2",
+                capacity: 4
+            },
+            {
+                table_name: "Patio #3",
+                capacity: 4
+            },
+            {
+                table_name: "Patio #4",
+                capacity: 4
+            },
+            {
+                table_name: "Patio #5",
+                capacity: 4
+            },
+            {
+                table_name: "Patio #6",
+                capacity: 4
+            },
           ]);
       })
   };
